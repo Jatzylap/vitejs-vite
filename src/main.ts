db7037eebeb54dd7274e87661c21b83c909801f1
Jatzylap <j4tzplayz@gmail.com>
2026-09-04 19:27:21 +0200

    new test

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ import html from './div_element.html?raw'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = html
     .replaceAll('${heroImg}', heroImg)
     .replaceAll('${typescriptLogo}', typescriptLogo)
-    .replaceAll('${viteLogo}', viteLogo)
+    .replaceAll('${viteLogo}', viteLogo);
 
 setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,1 @@
-export default {
-  base: '/vitejs-vite/'
-}
+export default {}
